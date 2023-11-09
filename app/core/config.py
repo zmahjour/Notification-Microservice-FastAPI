b2b0808f2e807fb89b313d4e3bc658f1b6c5edfc
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-09 17:22:40 +0330

    chore: define Settings class

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,10 @@
 from pydantic_settings import BaseSettings
 
 
+class Settings(BaseSettings):
+    class Config:
+        env_file = (
+            "/home/samane/Documents/MaktabSharif/FinalProject/Project/Notification/.env"
+        )
+
+
