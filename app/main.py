f1b602305cba44fbc83ebe3b446c3b1e144cbc70
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-09 17:23:50 +0330

    chore: import router as notif_router and include it

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 import uvicorn
+from api.api_v1.api import router as notif_router
+
+
 app = FastAPI()
+app.include_router(notif_router)
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8004, reload=True)
