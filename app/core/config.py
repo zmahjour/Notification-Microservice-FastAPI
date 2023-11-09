f2564ec501601615c66085a3f913cba0f526a635
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-09 23:58:02 +0330

    chore: define email configs

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,18 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # E-Mail
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+    USE_CREDENTIALS: bool
+    VALIDATE_CERTS: bool
+
     class Config:
         env_file = (
             "/home/samane/Documents/MaktabSharif/FinalProject/Project/Notification/.env"
