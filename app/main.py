1ba643864dd5924e0d112e0a3c9acc6bad40d847
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-09 23:59:37 +0330

    chore: define redis_lifespan

--- app/main.py
+++ app/main.py
@@ -6,8 +6,16 @@ from api.api_v1.api import router as notif_router
 from core.config import settings
 
 
+@asynccontextmanager
+async def redis_lifespan(app: FastAPI):
+    app.state.redis = await aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DB}"
+    )
+    yield
+    await app.state.redis.close()
 
-app = FastAPI()
+
+app = FastAPI(lifespan=redis_lifespan)
 app.include_router(notif_router)
 
 
