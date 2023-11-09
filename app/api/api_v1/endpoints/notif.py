7f5ca6362fdf4aa8e0839ed5100cb1d21634bb87
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-10 00:02:02 +0330

    chore: define conf as an instance of ConnectionConfig

--- app/api/api_v1/endpoints/notif.py
+++ app/api/api_v1/endpoints/notif.py
@@ -9,3 +9,17 @@ from utils.utils import create_otp
 router = APIRouter(prefix="/notif", tags=["notification"])
 
 
+conf = ConnectionConfig(
+    MAIL_USERNAME=settings.MAIL_USERNAME,
+    MAIL_PASSWORD=settings.MAIL_PASSWORD,
+    MAIL_FROM=settings.MAIL_FROM,
+    MAIL_PORT=settings.MAIL_PORT,
+    MAIL_SERVER=settings.MAIL_SERVER,
+    MAIL_FROM_NAME=settings.MAIL_FROM_NAME,
+    MAIL_STARTTLS=settings.MAIL_STARTTLS,
+    MAIL_SSL_TLS=settings.MAIL_SSL_TLS,
+    USE_CREDENTIALS=settings.USE_CREDENTIALS,
+    VALIDATE_CERTS=settings.VALIDATE_CERTS,
+)
+
+
